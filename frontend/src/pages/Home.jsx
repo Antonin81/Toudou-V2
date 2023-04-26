0ed58f1b545f14a1e658522717f8a8376e9aac1f
Antonin81 <antonin.pidet@gmail.com>
2023-04-26 17:43:17 +0200

    Connexion entre backend et frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,50 @@
+import { useEffect, useState } from "react";
 
 function Home() {
+  const[taskList, setTaskList]=useState([])
+  const[error, setError]=useState(false)
+
+  useEffect(()=>{
+    async function fetchTasks(){
+      try {
+        const response=await fetch('http://localhost:8000/tasks/incomplete')
+        const data=await response.json()
+        const tasks=data;
+        setTaskList(data.tasksList)
+        console.log("j'ai les datas!")
+      } catch (err) {
+        console.log("Il y a une erreur")
+        setError(true)
+      }
+    }
+    fetchTasks()
+    console.log(taskList)
+  },[])
+
   return (
     <div className="Home">
-      <header className="Home-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="Home-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Bonjour User!</p>
+      {
+      !error 
+        ? taskList.map((task)=>{
+          return(
+          <div>
+            <br />
+            <p>id de task : {task.id_task}</p>
+            <p>{task.title}</p>
+            <p>{task.is_done}</p>
+            <p>{task.description}</p>
+            <p>{task.end_date}</p>
+            <p>{task.duration}</p>
+            <p>{task.completionDate}</p>
+            <p>{task.task_category}</p>
+            <br />
+          </div>)
+        })
+        :(
+        <p>Il y a eu une erreur</p>
+        )
+      }
     </div>
   );
 }
