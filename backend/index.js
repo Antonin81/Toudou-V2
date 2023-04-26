f96f7c17b72576235db8f154ee6af0d6720486d1
Antonin81 <antonin.pidet@gmail.com>
2023-04-26 17:11:00 +0200

    Project initialisation

---
+++ backend/index.js
@@ -0,0 +1,53 @@
+const express=require('express');
+const app=express();
+const http = require('http').createServer(app);
+const mysql=require('mysql');
+const cors=require('cors');
+const bodyParser=require('body-parser');
+
+app.use(express.static('static'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cors({
+    origin: /http:\/\/localhost/
+}))
+
+
+
+// ouvre le port et le serveur
+
+http.listen(8000, () =>{
+    console.log("Je suis sur le port 8000");
+});
+
+//crée la connexion avec la bdd
+
+const db=mysql.createConnection({
+    host:"localhost",
+
+    user:"root",
+
+    password:"root",
+
+    database:"toudou"
+});
+
+//gère le cas où la connexion échoue
+
+db.connect(function(err){
+    if (err) throw err;
+    console.log("Connecté à la base de données");
+});
+
+//routage
+
+app.get('/tasks/incomplete', (req,res) => {
+    db.query("select * from tasks where is_done=false;", function(err, reponse){
+            if (err) throw err;
+            var locals={
+                tasksList:reponse
+            }
+            res.send(locals);
+    });
+    
+});
