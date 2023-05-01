a9f2500edcb9d0f40a9c254c5e7fd192a8548a73
Antonin81 <antonin.pidet@gmail.com>
2023-05-01 10:39:24 +0200

    Gestion du clic sur une task pour en afficher le contenu

--- frontend/src/components/Task/index.jsx
+++ frontend/src/components/Task/index.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react"
+import React from "react"
+
 function Task({id_task, title, description, end_date, duration, task_category, completionDate, niveau}){
+    const [showAll, setShowAll]=useState(false)
+
+    function handleClick(e){
+        setShowAll(!showAll)
+    } 
+
     return(
-        <div data-parentcat={task_category}>
+        <div data-parentcat={task_category} onClick={handleClick}>
             <br />
-            <p>Niveau : {niveau}</p>
-            <p>id de task : {id_task}</p>
             <p>titre : {title}</p>
-            <p>description : {description}</p>
-            <p>date limite : {end_date}</p>
-            <p>durée estimée : {duration}</p>
-            <p>catégorie : {task_category}</p>
-            {completionDate && <p>date de complétion : {completionDate}</p>}
+            {
+                showAll && (
+                    <React.Fragment>
+                        <p>Niveau : {niveau}</p>
+                        <p>id de task : {id_task}</p>
+                        <p>description : {description}</p>
+                        <p>date limite : {end_date}</p>
+                        <p>durée estimée : {duration}</p>
+                        <p>catégorie : {task_category}</p>
+                        {completionDate && <p>date de complétion : {completionDate}</p>}
+                    </React.Fragment>
+                )
+            }
+            
             <br />
         </div>
     )
